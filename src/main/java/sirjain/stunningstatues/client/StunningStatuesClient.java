0fe2decf92d327201a3032a180dffec845667220
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-12-08 15:01:55 -0500

    Some changes, error still persisting

---
+++ src/main/java/sirjain/stunningstatues/client/StunningStatuesClient.java
@@ -0,0 +1,10 @@
+package sirjain.stunningstatues.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StunningStatuesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
